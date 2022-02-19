6338e3f1c48af04c2c3154347a5acd93e189f6d4
Prakhar Doneria <gamesprotec@gmail.com>
2022-02-19 17:36:40 +0530

    Update Microsoft Mail.py

--- Microsoft Mail.py
+++ Microsoft Mail.py
@@ -5,3 +5,12 @@ from mailer import Mailer
 mail = Mailer(email='protecstudio@hotmail.com', password='your_password')
 mail.settings(provider=mail.MICROSOFT)
 mail.send(receiver='someone@example.com', subject='TEST', message='From Python!')
+
+# Using (status) Attribute 
+print(mail.status)
+
+# Example For One Receiver:
+if mail.status:
+  pass
+else:
+  pass
